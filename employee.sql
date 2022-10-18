6590f3c6d85e8648874964976d75d0e096dffcd6
SyedaMahin7 <syedamahin.xworkz@gmail.com>
2022-10-19 00:02:01 +0530

    sql

---
+++ employee.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE AUG_20;
+CREATE TABLE employees_name(emp_id int,empl_name varchar(20),position varchar(10),salary bigint(10),experiance int(10),gender varchar(10), native varchar(10), bloddgroup varchar(10));
+use AUG_20;
+INSERT INTO employees_name VALUE(1,'syeda','softw',2,800,'f','hospet','a');
+SELECT* FROM employees_name;
+INSERT INTO employees_name VALUE(2,'vaishnavi','tester',4,450000,'f','bellary','b');
+INSERT INTO employees_name VALUE(3,'sudha','software',1,50000,'f','tumkure','o');
+INSERT INTO employees_name VALUE(4,'anusha','backend',2,45000,'f','bengalore','o');
+INSERT INTO employees_name VALUE(5,'harshita','helper',2,8000,'f','rayachure','ab');
+INSERT INTO employees_name VALUE(6,'bhargavi','tester',2,30000,'f','hospet','a');
+INSERT INTO employees_name VALUE(7,'anand','software',3,850000,'m','venkatapura','a');
+INSERT INTO employees_name VALUE(8,'mubashir','tester',1,25000,'m','bengalore','o');
+INSERT INTO employees_name VALUE(9,'kavitha','software',8,800000,'f','rayachure','a');
+INSERT INTO employees_name VALUE(10,'keerthi','developer',3,45000,'f','chamara','o');
+INSERT INTO employees_name VALUE(11,'syeda','softw',2,800,'f','hospet','a');
+
+
